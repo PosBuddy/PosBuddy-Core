5d33bc2eaf5009f4f59cb6c479497f04b09fdc6d
jkarthaus <Joern.karthaus@gmail.com>
2024-02-07 22:19:15 +0100

    more code in value componenet

--- posbuddy-staff/src/app/add-value/add-value.component.ts
+++ posbuddy-staff/src/app/add-value/add-value.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, inject, TemplateRef} from '@angular/core';
+import {NgbAlert, NgbOffcanvas} from "@ng-bootstrap/ng-bootstrap";
+import {FormsModule} from "@angular/forms";
+import {ZXingScannerModule} from "@zxing/ngx-scanner";
 
 @Component({
   selector: 'app-add-value',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbAlert,
+    FormsModule,
+    ZXingScannerModule
+  ],
   templateUrl: './add-value.component.html',
   styleUrl: './add-value.component.css'
 })
 export class AddValueComponent {
+  private offcanvasService = inject(NgbOffcanvas);
+
   formValid: boolean = false;
   posBuddyId: string = "-";
-  surname = '';
+  value = '0';
+
+
+  send() {
+    if (this.posBuddyId != "-") {
+      this.formValid = true
+    }
+  }
+
+  onScanSuccess(scanResult: string) {
+    this.posBuddyId = scanResult;
+    this.formValid = true
+    this.offcanvasService.dismiss("success");
+  }
+
+  scanQRCode(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, {ariaLabelledBy: 'offcanvas-basic-title'})
+  }
 
 
 }
