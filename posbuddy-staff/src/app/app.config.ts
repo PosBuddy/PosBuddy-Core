68ea12b858c3370d422df5f58562db04a5c5e052
jkarthaus <Joern.karthaus@gmail.com>
2024-04-20 12:03:37 +0200

    add pwa

--- posbuddy-staff/src/app/app.config.ts
+++ posbuddy-staff/src/app/app.config.ts
@@ -1,12 +1,17 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, isDevMode} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {HttpClientModule} from "@angular/common/http";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
-  ]
+    importProvidersFrom(HttpClientModule),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
