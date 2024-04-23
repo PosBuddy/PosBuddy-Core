752e8ee6364da661aeda2377494f21ba9a81cb3f
jkarthaus <Joern.karthaus@gmail.com>
2024-04-23 08:02:59 +0200

    version without serviceWorker

--- posbuddy-staff/src/app/app.config.ts
+++ posbuddy-staff/src/app/app.config.ts
@@ -1,17 +1,18 @@
-import {ApplicationConfig, importProvidersFrom, isDevMode} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {HttpClientModule} from "@angular/common/http";
-import { provideServiceWorker } from '@angular/service-worker';
+import {provideServiceWorker} from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
+  // TODO : check service worker - > !isDevMode()
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
     provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
+      enabled: false,
+      registrationStrategy: 'registerWhenStable:30000'
     })
-]
+  ]
 };
