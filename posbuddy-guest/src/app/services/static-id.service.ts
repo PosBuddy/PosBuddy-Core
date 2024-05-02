75bb4e1eafa4946c5d9f8823dd0ce38477ed05ce
jkarthaus <Joern.karthaus@gmail.com>
2024-05-02 09:00:08 +0200

    add no cache for static data

--- posbuddy-guest/src/app/services/static-id.service.ts
+++ posbuddy-guest/src/app/services/static-id.service.ts
@@ -7,6 +7,9 @@ const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
     'rejectUnauthorized': 'false',
+    'Cache-Control': 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
+    'Pragma': 'no-cache',
+    'Expires': '0'
   })
 };
 
